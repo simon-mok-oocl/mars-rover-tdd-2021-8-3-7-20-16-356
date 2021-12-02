728c88ad999850f2ca6ea6981c9954a141a97244
moksi3 <simon.mok@oocl.com>
2021-12-02 16:28:16 +0800

    [mr][sm] feat: add test: should_Y_increase_when_l_given_n

---
+++ src/main/java/com/afs/tdd/Rover.java
@@ -0,0 +1,36 @@
+package com.afs.tdd;
+
+public class Rover {
+    private int x;
+    private int y;
+    private char dir;
+
+    public Rover()
+    {
+        this.x = 0;
+        this.y = 0;
+        this.dir = 'N';
+    }
+
+    public void exec(char comm)
+    {
+        if(comm == 'm')
+        {
+            move();
+        }
+        else
+        {
+            System.out.println("Illegal command");
+        }
+    }
+
+    private void move()
+    {
+        //if(this.dir == 'N') {this.y++;}
+    }
+
+    public String getLocation()
+    {
+        return String.format("%d %d %c" , this.x , this.y , this.dir);
+    }
+}
---
+++ src/test/java/com/afs/tdd/RoverTest.java
@@ -0,0 +1,21 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RoverTest {
+
+    @Test
+    public void should_Y_increase_when_l_given_n()
+    {
+        char input = 'm';
+        String expected = "1 0 N";
+        Rover rover = new Rover();
+
+        rover.exec('m');
+        String output = rover.getLocation();
+
+        assertEquals(expected , output);
+    }
+}
